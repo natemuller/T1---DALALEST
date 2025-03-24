d4e22fb3a1e609bc219f9cabfc077882d0b54238
natan gostosao <natan.muller@edu.pucrs.br>
2025-03-24 18:56:40 -0300

    implementação do metodo insertionSort do file InsertionSort implementado e testado

--- ORDENADORES/src/InsertionSort.java
+++ ORDENADORES/src/InsertionSort.java
@@ -1,13 +1,36 @@
 public class InsertionSort {
 
-    public static void insertionSort (double[] array) {
-
+    public static void insertionSort (long[] array) {
+        for (int i = 1; i < array.length; i++) {
+            long key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = key;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
     public static void main(String[] args) throws Exception {   
 
-        double array1[] = {2836, 9787, 658, 26532};
-        double array2[] = {68653, 265808, 567657, 4737673, 9069327, 80, 932858};
-        double array3[] = {66532676, 693268657665.00, 279866, 583653268, 2653};
-        double array4[] = {2836, 9787, 658, 26532, 68653, 265808, 567657, 4737673, 9069327, 80, 932858, 66532676, 693268657665.00, 279866, 583653268, 2653};
+        long array1[] = {2836, 9787, 658, 26532};
+        long array2[] = {68653, 265808, 567657, 4737673, 9069327, 80, 932858};
+        long array3[] = {66532676, 693268657665l, 279866, 583653268, 2653};
+        long array4[] = {2836, 9787, 658, 26532, 68653, 265808, 567657, 4737673, 9069327, 80, 932858, 66532676, 693268657665L, 279866, 583653268, 2653};
+
+        System.out.printf("array1:\n");
+        insertionSort(array1);
+        System.out.printf("\n"); 
+        System.out.printf("array2:\n");
+        insertionSort(array2);
+        System.out.printf("\n");
+        System.out.printf("array3:\n");
+        insertionSort(array3);
+        System.out.printf("\n");
+        System.out.printf("array4:\n");
+        insertionSort(array4);
     }
 }
